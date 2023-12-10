88ac6a4a0c9aaff043af56758db5d1bf9da49cc3
bartek-kachnic123 <you@example.com>
2023-12-10 18:06:14 +0100

    Added logo to SearchView

---
+++ src/components/search/SearchLogo.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="text-center">
+  <img src="@/assets/logo.svg" class="img-thumbnail" alt="logo" id="logo">
+  </div>
+</template>
+
+<style scoped>
+#logo {
+  max-width: 200px;
+}
+</style>
--- src/views/SearchView.vue
+++ src/views/SearchView.vue
@@ -3,12 +3,13 @@
 import NavbarTop from "@/components/navbar/NavbarTop.vue";
 import NavbarBottom from "@/components/navbar/NavbarBottom.vue";
 import SearchBar from "@/components/search/SearchBar.vue";
+import SearchLogo from "@/components/search/SearchLogo.vue";
 </script>
 
 <template>
   <NavbarTop />
   <NavbarBottom />
-  <h1 class="text-center mt-3">Search View</h1>
+  <SearchLogo />
 
   <div class="text-md-center mt-3">
     <SearchBar />
