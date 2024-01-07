7ff22d112ab2d9dc2bf8ab44df0222df156a0980
Ech0n <43513980+Ech0n@users.noreply.github.com>
2024-01-07 20:50:17 +0100

    Added course-add logic

--- src/views/AddCourseView.vue
+++ src/views/AddCourseView.vue
@@ -1,6 +1,12 @@
 <script setup>
 import NavbarTop from "@/components/navbar/NavbarTop.vue";
+import { openDB } from 'idb';
+import {ref} from 'vue'
 
+let category = ref("")
+let price = ref(0)
+let description = ref("")
+let name = ref("")
 function onPickFile () {
   this.$refs.fileInput.click()
 }
@@ -16,6 +22,37 @@ function onFilePicked (event) {
   this.image = files[0]
 }
 
+
+async function pushToDB(course)
+{
+  console.log(course)
+  const dbs = await openDB("db_",1);
+  await dbs.put("courses",course)
+  dbs.close()
+}
+
+async function getUserData(){
+  const db = await openDB("db_",1);
+  let username = sessionStorage.getItem("username")
+  let res = await db.getFromIndex('users','findUsername',username)
+  db.close()
+
+  return res
+}
+
+
+function addCourse(){
+  const currentDate = new Date();
+  getUserData().then((userData) =>
+  {
+    pushToDB({name:name.value,short_desc:description.value,last_updated:currentDate.toISOString().split('T')[0],
+    authorId:userData.id,
+    author_name: userData.firstname + userData.lastname,
+    price:price.value
+  })
+})
+}
+
 </script>
 
 <template>
@@ -24,12 +61,12 @@ function onFilePicked (event) {
   <div class="text-md-left ml5">
     
     <div class="row">
-        <h1><input type="name" placeholder="Nazwa kursu"></h1>
+        <h1><input type="name" v-model="name" placeholder="Nazwa kursu"></h1>
         
         <div class="col">
             <h3>
                 Kategoria kursu:<br>
-                <select v-model="selected">
+                <select v-model="category">
                     <option disabled value="">Wybierz Kategorię</option>
                     <option>Python</option>
                     <option>E-commerce</option>
@@ -38,7 +75,7 @@ function onFilePicked (event) {
                     <option>Algorytmy</option>
                 </select>
                 <br>
-                <button @click="onPickFile">Upload profile picture</button>
+                <button @click="onPickFile">Upload course thumbnail</button>
                 <input
                     type="file"
                     style="display: none"
@@ -51,16 +88,16 @@ function onFilePicked (event) {
         <div class="col">
             <h3>
                 Cena kursu:<br>
-                <input type="price" placeholder="zł"><br>
+                <input type="price" placeholder="zł" v-model="price"><br>
 
                 Opis kursu:<br>
-                <textarea v-model="message" placeholder="Opisz swoją wersję kursu najlepiej jak potrafisz..."></textarea>
+                <textarea v-model="description" placeholder="Opisz swoją wersję kursu najlepiej jak potrafisz..."></textarea>
 
             </h3>
         </div>
     </div>
 
-    <h2><button>Dodaj kurs</button></h2>
+    <h2><button @click="addCourse">Dodaj kurs</button></h2>
   </div>
 </template>
 
