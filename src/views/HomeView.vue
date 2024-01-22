4dff6cc69744afb98403f4fd0f1bc6f22e929120
Onarix <pketk@op.pl>
2024-01-22 22:10:59 +0100

    Added welcome screen to logged user in home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,17 +2,40 @@
 import NavbarTop from "@/components/navbar/NavbarTop.vue";
 import SignUpSelectorCard from "../components/card/SignUpSelectorCard.vue";
 
+
 let isLoggedIn = false
 if(sessionStorage.getItem("userId"))
 {
   isLoggedIn = true
 }
+
+let username = sessionStorage.getItem("username");
+
+async function loadData() {
+    const db = await openDB("db_",1);
+    let k = await db.get('users',parseInt(sessionStorage.getItem("userId")));
+    console.log(k["class"]);
+    userData.value = k;
+
+    db.close()
+}    
+
+// TODO: zaimplementować
+function isTeacher() {
+  return false;
+}
+
+
 </script>
 
 <template>
   <NavbarTop />
   <!-- <NavbarBottom /> -->
+
   <div class = "d-flex flex-column align-items-center justify-content-center">
+  <h1 v-if="isLoggedIn">Witaj, {{ username }}.</h1>
+  <div class="d-flex p-2"><mdicon v-if="isTeacher() && isLoggedIn" name="school" :width="150" :height="200" /></div>
+  <div class="d-flex p-2"><mdicon v-if="!(isTeacher()) && isLoggedIn" name="account" :width="150" :height="200" /></div>
   <h1 class="text-center">Wyszukiwanie:</h1>
   <div class="d-flex p-2">
     <a href="/search?type=courses" type="button" class="btn btn-primary">Szukaj Kursu</a>
